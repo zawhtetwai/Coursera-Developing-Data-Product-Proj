0a03a92b593e594d0b4abca3f74f79014ac78369
zawhtetwai <zawhtet@tp.edu.sg>
2015-04-26 06:02:11 +0800

    touched up on Help

--- ui.R
+++ ui.R
@@ -5,16 +5,21 @@ shinyUI(fluidPage(
 
   # Sidebar 
   sidebarLayout(
-    sidebarPanel(
-	uiOutput("country"),
-      uiOutput("year"), 
-      helpText("Data reflects the life expectancy ", 
-               "for male and female at birth and at age of 60s.", 
-               "The analysis covers from year 1990 to 2012 for all countries. ", 
-                "Data for this project are obtained ", 
-               "from the Global Health Observatory (GHO).", 
-                "http://apps.who.int/gho/data/node.main.3?lang=en")              
-    ),
+   sidebarPanel(
+   uiOutput("country"),
+   uiOutput("year"),      
+   helpText("Self Service Life Expectancy Analysis"),
+   helpText("This application aims to provide you with the life expectancy insight", 
+            "for all over the countries from year 1990 to 2012."), 
+   helpText("Step. 1: Choose the country you are interested. "), 
+   helpText("Step. 2: Move the slider for the year range (between 1990 and 2012)."), 
+   helpText("There are two plots in the main screen (right). ",
+		"Top is for Life Expectancy at Birth",
+		"Bottom is for Life Expectancy at 60"),
+   helpText("Data for this project are obtained ", 
+            "from the Global Health Observatory (GHO).", 
+            "http://apps.who.int/gho/data/node.main.3?lang=en")
+	),
 
     # Show the plot 
     mainPanel(
